6e264868f72351cb9d246f5b5468d1ba2c934370
Miuroro <monera.mual@gmail.com>
2026-07-27 00:21:57 +0200

    feat/ added R2 storage service for file uploads

---
+++ src/main/java/com/hyfbackend/miniproject/service/R2StorageService.java
@@ -0,0 +1,34 @@
+package com.hyfbackend.miniproject.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+public class R2StorageService {
+
+    private final S3Client s3Client;
+
+    @Value("${cloudflare.r2.bucket-name}")
+    private String bucketName;
+
+    public R2StorageService(S3Client s3Client) {
+        this.s3Client = s3Client;
+    }
+
+    public String uploadFile(MultipartFile file) throws IOException {
+        String fileName = UUID.randomUUID() + "-" + file.getOriginalFilename();
+        s3Client.putObject(PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(fileName)
+                .contentType(file.getContentType())
+                .build(), RequestBody.fromBytes(file.getBytes()));
+        return fileName;
+    }
+}
